c282ffc15c4f1bfec8d0159ad2bd4f0be2e0862f
Ray Baxter <ray.baxter@gmail.com>
2008-12-24 00:05:19 -0800

    authentication and connection

--- lib/gdata_service.rb
+++ lib/gdata_service.rb
@@ -1,4 +1,5 @@
 require 'net/https'
+require 'uri'
 
 class Service
   attr_reader :headers
@@ -10,44 +11,62 @@ class Service
     @login_url ||='http://www.google.com:443/accounts/ClientLogin'    
   end
   
-	def authenticate(login_url='http://www.google.com:443/accounts/ClientLogin')
-  
+	def client_authenticate(login_url='http://www.google.com:443/accounts/ClientLogin')
+
     data = "accountType=HOSTED_OR_GOOGLE&Email=#{@user}&Passwd=#{@password}&service=#{@service}"
     
-    response = post_data(login_url, data)
-    if response.kind_of?(Net::HTTPSuccess)
-    	cl_string = response.body[/Auth=(.*)/, 1]
-    	@headers = { 
-    		  'Content-Type' => 'application/atom+xml', 
-    			'Accept' => 'application/atom+xml',
-    			'Source' => '67central.com--get-feed-0.0.1',
-    			'Authorization' => "GoogleLogin auth=#{cl_string}" 
-    	}
-    	response
-    else 
-      # TODO - raise
-      # Error Codes  http://code.google.com/apis/accounts/docs/AuthForInstalledApps.html#Errors
-      # Error Code	Description
-      # BadAuthentication 	The login request used a username or password that is not recognized.
-      # NotVerified 	The account email address has not been verified. The user will need to access their Google account directly to resolve the issue before logging in using a non-Google application.
-      # TermsNotAgreed 	The user has not agreed to terms. The user will need to access their Google account directly to resolve the issue before logging in using a non-Google application.
-      # CaptchaRequired 	A CAPTCHA is required. (A response with this error code will also contain an image URL and a CAPTCHA token.)
-      # Unknown 	The error is unknown or unspecified; the request contained invalid input or was malformed.
-      # AccountDeleted 	The user account has been deleted.
-      # AccountDisabled 	The user account has been disabled.
-      # ServiceDisabled 	The user's access to the specified service has been disabled. (The user account may still be valid.)
-      # ServiceUnavailable 	The service is not available; try again later.
-    	puts response
-    	puts response.body
+    begin
+      response = post_data(login_url, data)
+    rescue GDataException => e
+      return humanized_message(e.message)
     end
+    	
+    cl_string = response.body[/Auth=(.*)/, 1]
+    @headers = {  'Content-Type' => 'application/atom+xml', 
+    	            'Accept' => 'application/atom+xml',
+    	            'Source' => '67central.com--get-feed-0.0.1',
+    	            'Authorization' => "GoogleLogin auth=#{cl_string}" }
+  end
+
+
+  def humanized_message(string)
+    # Error Codes  http://code.google.com/apis/accounts/docs/AuthForInstalledApps.html#Errors
+    case error_message_from(string)
+      when "BadAuthentication"	
+        "The login request used a username or password that is not recognized."
+      when "NotVerified"
+        "The account email address has not been verified. The user will need to access their Google account directly to resolve the issue before logging in using a non-Google application."
+      when "TermsNotAgreed" 	
+        "The user has not agreed to terms. The user will need to access their Google account directly to resolve the issue before logging in using a non-Google application."
+      when "CaptchaRequired" 	
+        "A CAPTCHA is required. You should login to your account on the web and try this request again."
+      when "Unknown"
+        "The error is unknown or unspecified; the request contained invalid input or was malformed."
+      when "AccountDeleted"
+        "The user account has been deleted."
+      when "AccountDisabled"	
+        "The user account has been disabled."
+      when "ServiceDisabled"
+        "The user's access to the specified service has been disabled. (The user account may still be valid.)"
+      when "ServiceUnavailable"
+        "The service is not available; try again later."
+      else
+        ""
+      end
   end
   
-  def g_request(method="GET", uri_string=nil, data=nil, limit = 5)
+  def error_message_from(string)
+    # Real life examples!
+    # "CaptchaToken=YPpW9Tx9DNyqLxCe7AZPGfgK5B-gsKe2vF8RKPZlYpOyXnl7uMUM3OzpXwMZ42FmaSBedzU4hG9RejytwEXzlmRJfGcocLj42k8ylTiwGbyTDNPe6zsnB4EjH7uIz8mU:9Pj70SHQ6ffjXhGC9pT9uw\nCaptchaUrl=Captcha?ctoken=YPpW9Tx9DNyqLxCe7AZPGfgK5B-gsKe2vF8RKPZlYpOyXnl7uMUM3OzpXwMZ42FmaSBedzU4hG9RejytwEXzlmRJfGcocLj42k8ylTiwGbyTDNPe6zsnB4EjH7uIz8mU%3A9Pj70SHQ6ffjXhGC9pT9uw\nError=CaptchaRequired\nUrl=https://www.google.com/accounts/ErrorMsg?Email=calendar.mafasdfadssaddsaven%40gmail.com&service=cl&id=cr&timeStmp=1229575266&secTok=a6928d058068e77a6ab544f10270ccb0\n"
+    # "Error=BadAuthentication\n"
+    string.split(/\n/).detect{|s| s =~ /^Error=/}.split(/=/).last    
+  end
+  
+  def g_request(method, uri_string, data=nil, limit = 5)
   	raise "Too many redirects!" if limit <= 0 
   
   	uri = URI.parse(uri_string)
   	@http ||= Connection.new('www.google.com','443')
-
 
   	# Something like method = method.downcase.to_sym; @http.method() could work here.
   	response = case method
@@ -61,9 +80,17 @@ class Service
   							 @http.delete(uri.request_uri, @headers)
   						 end 
   
-  	return response if response.kind_of?(Net::HTTPSuccess)
-  	return g_request(method, response['location'], data, limit-1)  if response.kind_of?(Net::HTTPRedirection)
-  	return response.error!  
+    # require 'ruby-debug'; debugger
+
+    if response.kind_of?(Net::HTTPRedirection)
+      return g_request(method, response['location'], data, limit-1)
+    elsif response.kind_of?(Net::HTTPForbidden)
+      raise GDataException.new(response.body)
+    elsif response.kind_of?(Net::HTTPSuccess)
+      return response
+    else
+  	  return response.error!
+	  end
   end
   
   def get_data(uri_string, limit = 5)
@@ -86,19 +113,31 @@ end
 
 class Connection < Net::HTTP
   
-	def initialize(address, port = 443, ca_file = "/usr/share/curl/curl-ca-bundle.crt")
+	def initialize(address, port = 443)
+	  
 		super(address,port)
+		
+		self.ca_file = ENV['ca_file'] ? ENV['ca_file'] : "config/curl-ca-bundle.crt"
 		self.use_ssl = true
-    # self.set_debug_output $stderr if DEBUG
 		if File.exists?(ca_file)
-      # @@logger.debug "ca_file exists"
-			self.ca_file = "/usr/share/curl/curl-ca-bundle.crt"
 			self.verify_mode = OpenSSL::SSL::VERIFY_PEER
 			self.verify_depth = 5
 		else
-      # @@logger.warn "No certificate file"
+      warn "No certificate file. Expected #{ca_file} to exist. Continuing..." unless ca_file.nil?
 			self.verify_mode = OpenSSL::SSL::VERIFY_NONE
 		end
 	end
 	
+end
+
+class GDataException < StandardError
+  
+  def initialize(string)
+    @string = string
+  end
+
+  def message
+    @string
+  end
+  
 end
--- spec/lib/gdata_service_spec.rb
+++ spec/lib/gdata_service_spec.rb
@@ -1,13 +1,23 @@
 require 'lib/gdata_service'
+require 'net/https'
 
 # require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Service do
+  
+  def observed_responses_to_bad_logins
+    [
+      "The login request used a username or password that is not recognized.",
+      "A CAPTCHA is required. You should login to your account on the web and try this request again.",
+    ]
+  end
+  
   it "should initialize" do
     Service.new("user","password").should_not be_nil
   end
   
   describe "#authentication with valid credentials" do
+    
     before(:all) do
       p "This test block requires valid authorization credentials and a live Internet connection to Google's service"
     end
@@ -16,44 +26,139 @@ describe Service do
       @service = Service.new('calendar.maven','complic*')
     end
       
-    it "should authenticate" do
-      @service.authenticate.should_not be_nil
+    it "should client_authenticate" do
+      @service.client_authenticate.should_not be_nil
     end
     
     it "should expose the headers" do
-      @service.authenticate
+      @service.client_authenticate
       @service.headers.should_not be_nil
     end
     
     it "should set a valid authorization token" do
-      @service.authenticate
+      @service.client_authenticate
       @service.headers['Authorization'].should match(/GoogleLogin auth=/)
       @service.headers['Authorization'].size.should == "GoogleLogin auth=DQAAAHgAAABMZexg9VNaQhrvMDRpl16GOMMh1MIiZ9QlNRibQsE7n8la0Thdzv38dffaoiKaBwkXDA9hLjftiofFAsaKz_aM5VkEUx_qEoWUy_J-gHfUg7-APU2pyvuRwt5xIYppFUlDh-kesuj7s3jv6RGXMx6vKdP20Z2VBhYG3OdTyljfng".size      
     end
     
   end
 
-  describe "#authentication with invalid credentials" do
+  describe "#authentication with valid user and invalid password" do
+    
+    it "should not client_authenticate" do
+      service = Service.new("calendar.maven",'password')
+      observed_responses_to_bad_logins.should include(service.client_authenticate)
+     end
+  end
+
+  describe "#authentication with an invalid user" do
+    
     before(:all) do
       p "This test block requires invalid authorization credentials and a live Internet connection to Google's service"
     end
 
-    before(:each) do
-      @service = Service.new('I_should_never_be_a_valid_user_name','password')
-    end
-      
-    it "should not authenticate" do
-      lambda {  @service.authenticate }.should raise_error
+    it "should respond with a known error message" do
+      service = Service.new("not_a_valid_user#{Time.now.to_i}",'password')
+      observed_responses_to_bad_logins.should include(service.client_authenticate)
     end
         
   end
+  
+  describe "#error_message_from" do
     
+    before(:all) do
+      @service = Service.new("user","password")
+    end
+        
+    it "should take a simple string and return a string value" do
+      @service.error_message_from("Error=BadAuthentication\n").should == "BadAuthentication"
+    end
+    
+    it "should take a complex string and return a string value" do
+      @service.error_message_from("CaptchaToken=YPpW9Tx9DNyqLxCe7AZPGfgK5B-gsKe2vF8RKPZlYpOyXnl7uMUM3OzpXwMZ42FmaSBedzU4hG9RejytwEXzlmRJfGcocLj42k8ylTiwGbyTDNPe6zsnB4EjH7uIz8mU:9Pj70SHQ6ffjXhGC9pT9uw\nCaptchaUrl=Captcha?ctoken=YPpW9Tx9DNyqLxCe7AZPGfgK5B-gsKe2vF8RKPZlYpOyXnl7uMUM3OzpXwMZ42FmaSBedzU4hG9RejytwEXzlmRJfGcocLj42k8ylTiwGbyTDNPe6zsnB4EjH7uIz8mU%3A9Pj70SHQ6ffjXhGC9pT9uw\nError=CaptchaRequired\nUrl=https://www.google.com/accounts/ErrorMsg?Email=calendar.mafasdfadssaddsaven%40gmail.com&service=cl&id=cr&timeStmp=1229575266&secTok=a6928d058068e77a6ab544f10270ccb0\n").should == "CaptchaRequired"
+    end    
+    
+  end
+    
+  describe "#g_request" do
+    
+    before(:all) do
+      @connection = Connection.stub!(:new)
+      @service = Service.new("user","password")
+    end
+
+    it "should call @connection.get" do
+      pending("Better stubbing")
+      @connection.should_receive(:get).with("/").and_return(Net::HTTPSuccess.new)
+      @service.g_request("GET","http://www.example.com/")
+    end
+    
+  end
+
 end
 
 describe Connection do
   
+  it "should inherit from Net::HTTPS" do
+    Connection.superclass == "Net::HTTPS"
+  end
+  
   it "should initalize with hostname" do
     Connection.new('example.com').should_not be_nil
   end
+  
+  it "should set use_ssl to true" do
+    Connection.new('example.com').use_ssl.should be_true
+  end
+  
+  it "should allow setting the port" do
+    Connection.new('example.com',1234).port.should == 1234
+  end
+  
+  it "should use the default ca_file" do
+    Connection.new('example.com').ca_file.should == 'config/curl-ca-bundle.crt'
+  end
+  
+  it "should allow setting the ca_file in the environment" do
+    ENV['ca_file'] = '09876'
+    Connection.new('example.com').ca_file.should == '09876'
+    ENV.delete('ca_file') # Otherwise breaks if tests run in reverse!
+  end  
+  
+  it "should warn if the ca_file is not found unless it was nil" do
+    pending("Checking on change of $stderr")
+    ca_file = 'sakj'
+    Object.should_receive(:warn, "No certificate file. Expected #{ca_file} to exist. Continuing...")
+    Connection.new('example.com',123,ca_file)
+  end
 
+  it "should not warn if the ca_file is not found if location is nil" do
+    pending("Checking on change of $stderr")
+    pending("passes without reason")
+    $stderr = StringIO.new
+    lambda { Connection.new('example.com',123,nil)}.should_not change($stderr,:size)
+  end
+  
+  it "should allow setting debug_output" do
+    Connection.new('example.com').set_debug_output(true).should be_true
+    Connection.new('example.com').set_debug_output($stderr).should == $stderr
+  end
+  
+end
+
+describe GDataException do
+  
+  it "should inherit from StandardError" do
+    GDataException.superclass == StandardError
+  end
+
+  it "should initialize with a string" do
+    GDataException.new("string").should_not be_nil
+  end
+  
+  it "should return passed in string as #message" do
+    string = "Hi Mom"
+    GDataException.new(string).message.should == string
+  end
+    
 end
