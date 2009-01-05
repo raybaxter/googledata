943840a3f02ba45927566eee790b5bf877947956
Ray Baxter <ray.baxter@gmail.com>
2009-01-04 21:19:15 -0800

    File rename

--- spec/lib/gcal/calendars_spec.rb
+++ spec/lib/gcal/calendars_spec.rb
@@ -1,17 +1,17 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require 'spec/lib/atom/common_elements'
  
-describe GCal::CalendarsFeed do
+describe GCal::Calendars do
   before(:all) do
-    @feed = GCal::CalendarsFeed.file("spec/fixtures/feeds/smallcalendars.xml")
+    @feed = GCal::Calendars.file("spec/fixtures/feeds/smallcalendars.xml")
   end
 
   it "should read a calendar" do
-    GCal::CalendarsFeed.file("spec/fixtures/feeds/smallcalendars.xml").should_not be_nil
+    GCal::Calendars.file("spec/fixtures/feeds/smallcalendars.xml").should_not be_nil
   end
 
   it "should be a subclass of AtomFeed" do
-    GCal::CalendarsFeed.superclass.should == Atom::Container
+    GCal::Calendars.superclass.should == Atom::Container
   end
   
   it_should_behave_like "common Atom elements"  
@@ -20,18 +20,18 @@ end
 
 describe "GData Elements" do
   before(:all) do
-    @calendars = GCal::CalendarsFeed.file("spec/fixtures/feeds/allcalendars.xml")
+    @calendars = GCal::Calendars.file("spec/fixtures/feeds/allcalendars.xml")
   end
   
   it "should have #calendar_feeds" do
-    @calendars.calendar_feeds.size.should == 11
+    @calendars.calendars.size.should == 11
   end
   
 end
 
 describe "links" do
   before(:all) do
-    @calendars = GCal::CalendarsFeed.file("spec/fixtures/feeds/smallcalendars.xml")
+    @calendars = GCal::Calendars.file("spec/fixtures/feeds/smallcalendars.xml")
   end
   
   it "should have an alternate link" do
